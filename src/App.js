c4a9834d6c69cb7f46c414badb23b2d246f86a5e
albixhuveli <albi.xhuveli.601@gmail.com>
2023-05-03 01:37:06 -0400

    new try

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -5,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
@@ -16,7 +18,4 @@ export default function App() {
       <Contact />
     </main>
   );
-}
-export default function App() {
-  return "App";
 }
